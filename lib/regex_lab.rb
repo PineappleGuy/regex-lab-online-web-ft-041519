db73830e7e3b1b2bbf30dfda9ecb4d73783cada1
Garth Lilly <garff247@gmail.com>
2019-04-26 06:36:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -47,9 +47,9 @@ y = 0
 array = phone.split("")
 array.each do |x|
 
-  if x == 1||x == 2||x == 3||x == 4||x ==5||x == 6||x == 7||x == 8||x == 9||x == 0|
+  if x == "1"||x == "2"||x == "3"||x == "4"||x == "5"||x == "6"||x == "7"||x == "8"||x == "9"||x == "0"|
     y += 1
-    puts x
+
   end
 end
   if y == 10
