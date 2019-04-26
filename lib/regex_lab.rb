c40051dbc8d0fb776c43a06b3f53d11f9c2f951c
Garth Lilly <garff247@gmail.com>
2019-04-26 06:32:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -50,10 +50,11 @@ array.each do |x|
   if x == 1||x == 2||x == 3||x == 4||x ==5||x == 6||x == 7||x == 8||x == 9||x == 0|
     y += 1
   end
+end 
   if y == 10
     return true
   else
     return false
   end
 end
-end
+
