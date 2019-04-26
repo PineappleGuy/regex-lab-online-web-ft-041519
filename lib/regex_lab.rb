926492f4459843c094d0385d0500d6faba554610
Garth Lilly <garff247@gmail.com>
2019-04-26 05:20:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -45,7 +45,7 @@ def valid_phone_number?(phone)
 #  phone.match?(/(\d{3})\d{3}\d{4}/)
 y = 0
 phone.loop do |x|
-  if x == 1||2||3||4||5||6||7||8||9||0
+  if x == 1|| x == 2|| x == 3|| x == 4|| x == 5|| x == 6|| x == 7|| x == 8|| x == 9|| x == 0
     y += 1
   end
 end
