1d93aeae329c3e7cc44afa880d5e789cafa98435
Garth Lilly <garff247@gmail.com>
2019-04-26 06:29:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -50,7 +50,7 @@ array.each do |x|
   if x == 1||x == 2||x == 3||x == 4||x ==5||x == 6||x == 7||x == 8||x == 9||x == 0|
     y += 1
   end
-  if y = 10
+  if y == 10
     return true
   else
     return false
